b4b86981386e0159169418f51e0bae18dcde40a1
Leon W. <orlando.game@web.de>
2026-03-16 18:12:36 +0100

    added JSON and init function in html

---
+++ scripts/db.js
@@ -0,0 +1,98 @@
+let DishesDB = [
+    {
+        "category": "Burger & Sandwich",
+        "dishes": [
+            {
+                "name": "Veggie mushroom black burger",
+                "description": "Mixed green, Tomatoes, Edamame",
+                "price": 16.90,
+                "image": "black_burger.jpg",
+            },
+            {
+                "name": "All meat burger",
+                "description": "Beef, Bacon, Dill pickels, Smkoed cheese, Ketchup, BBQ souse",
+                "price": 15.90,
+                "image": "meat_burger.jpg",
+            },
+            {
+                "name": "Beef red burger",
+                "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                "price": 14.90,
+                "image": "red_burger.jpg",
+            },
+            {
+                "name": "Big chicken burger",
+                "description": "Chicken, Cheese, Tomaoes, Lettuce, onion, Bell, Pepper",
+                "price": 15.90,
+                "image": "chicken_burger.jpg",
+            },
+        ]
+    },
+    {
+        "category": "Pizza",
+        "dishes": [
+            {
+                "name": "Pizza margherita",
+                "description": "Tomato Suace, Mozzarella",
+                "price": 11.90,
+                "image": "pizza_margherita.jpg",
+            },
+            {
+                "name": "Pizza Chorizo",
+                "description": "Tomato slice, Mozzarello, Chorizo",
+                "price": 13.90,
+                "image": "pizza_chorizo.jpg",
+            },
+            {
+                "name": "Funghi",
+                "description": "Red Onion, Olives, Button Mushrooms, Mozzarello",
+                "price": 12.90,
+                "image": "pizza_funghi.jpg",
+            },
+            {
+                "name": "Quatro Formaggi with Chicken",
+                "description": "Chicken, Mozzarello, Gorgonzola, Fontina, Parmigiano Reggiono",
+                "price": 15.90,
+                "image": "pizza_chickenformaggi.jpg",
+            },
+        ]
+    },
+    {
+
+        "category": "Salad",
+        "dishes": [
+            {
+                "name": "Warm beef argula sald",
+                "description": "Beef, Argula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                "price": 16.90,
+                "image": "salad_arugula.jpg",
+            },
+            {
+
+                "name": "Mini green Salad",
+                "description": "Green salad, cucumber, Carrots, Parslex, Radishes",
+                "price": 7.90,
+                "image": "salad_mini.jpg",
+            },
+            {
+
+                "name": "Green Salad with Sea food",
+                "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustardlemon dressing with dill",
+                "price": 16.90,
+                "image": "salad_seafood.jpg",
+            },
+            {
+
+                "name": "Vegan green salad with tofu",
+                "description": "Green salad, Cherry tomatoes, Cucumber, baby Spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                "price": 14.90,
+                "image": "salad_vegan.jpg",
+            },
+        ]
+    }
+]
+
+let cartShopping = [
+
+]
+
